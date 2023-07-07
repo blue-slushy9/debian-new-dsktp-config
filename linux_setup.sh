e8890eb3a06a8c3ea3382ba5e4a694e61c0f3973
blue-slushy9 <ra.cruz971@gmail.com>
2023-07-07 10:50:40 -0700

    This should make setting up my new Linux machines very easy! I pretty much always install the same things, so this should make the process of distro-hopping much more streamlined.

---
+++ linux_setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#
+# This is a Bash script I plan on using to automate the setup of my Linux
+# machines, since I tend to go through many of them, both on bare metal and
+# virtual machine;
+#
+# 
